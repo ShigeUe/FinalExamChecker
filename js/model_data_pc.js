30c09b3e8408348e77e07ce25b18961547f398ef
UENISHI Shigefumi <16932213+ShigeUe@users.noreply.github.com>
2023-11-04 23:20:43 +0900

    さらに修正

--- js/model_data_pc.js
+++ js/model_data_pc.js
@@ -571,7 +571,7 @@ const MODEL_DATA_PC = [
     ],
     "property": {
       "color": "rgb(102, 102, 102)",
-      "fontSize": "10px",
+      "fontSize": "12px",
       "fontWeight": "400",
       "fontStyle": "normal",
       "fontFamily": "Noto Sans JP",
@@ -667,7 +667,7 @@ const MODEL_DATA_PC = [
     ],
     "property": {
       "color": "rgb(102, 102, 102)",
-      "fontSize": "10px",
+      "fontSize": "12px",
       "fontWeight": "400",
       "fontStyle": "normal",
       "fontFamily": "Noto Sans JP",
