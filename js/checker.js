2cd408beba18a45d144b6fb9cca8b42c43f8e8aa
UENISHI Shigefumi <16932213+ShigeUe@users.noreply.github.com>
2025-04-28 17:59:48 +0900

    微調整

--- js/checker.js
+++ js/checker.js
@@ -123,259 +123,261 @@
 
   const result = document.getElementById('result');
 
-  document.querySelectorAll('.perform').forEach((button) => {
-    button.addEventListener('click', async (e) => {
-      const row_html = document.getElementById('html').value;
-      const html = numbering(row_html.split(/\n/));
-      const css = numbering(document.getElementById('css').value.split(/\n/));
-      const js = numbering(document.getElementById('js').value.split(/\n/));
+  const buttonOnClick = async () => {
+    const row_html = document.getElementById('html').value;
+    const html = numbering(row_html.split(/\n/));
+    const css = numbering(document.getElementById('css').value.split(/\n/));
+    const js = numbering(document.getElementById('js').value.split(/\n/));
 
-      result.innerHTML = '';
+    result.innerHTML = '';
 
-      const doc = document.implementation.createHTMLDocument("").documentElement;
-      doc.innerHTML = row_html;
+    const doc = document.implementation.createHTMLDocument("").documentElement;
+    doc.innerHTML = row_html;
 
-      let results;
-      let errorMessage = '';
+    let results;
+    let errorMessage = '';
 
-      comment('■HTMLエラーチェック');
-      errorMessage = await html_error_check();
-      if (errorMessage) {
-        write(`<span class="red">${errorMessage}</span>`, true);
-        // write(`<button type="button" class="validator" onclick="document.forms['html-check-form'].submit();return false;">Validatorを開く</button>`, true);
-      }
-      else {
-        write(`<span class="blue">エラーはありません</span>`, true);
-      }
+    comment('■HTMLエラーチェック');
+    errorMessage = await html_error_check();
+    if (errorMessage) {
+      write(`<span class="red">${errorMessage}</span>`, true);
+      // write(`<button type="button" class="validator" onclick="document.forms['html-check-form'].submit();return false;">Validatorを開く</button>`, true);
+    }
+    else {
+      write(`<span class="blue">エラーはありません</span>`, true);
+    }
 
-      comment('■CSSエラーチェック');
-      errorMessage = await css_error_check();
-      if (errorMessage) {
-        write(`<span class="red">${errorMessage}</span>`, true);
-        // write(`<button type="button" class="validator" onclick="document.forms['css-check-form'].submit();return false;">Validatorを開く</button>`, true);
-      }
-      else {
-        write(`<span class="blue">エラーはありません</span>`, true);
-      }
+    comment('■CSSエラーチェック');
+    errorMessage = await css_error_check();
+    if (errorMessage) {
+      write(`<span class="red">${errorMessage}</span>`, true);
+      // write(`<button type="button" class="validator" onclick="document.forms['css-check-form'].submit();return false;">Validatorを開く</button>`, true);
+    }
+    else {
+      write(`<span class="blue">エラーはありません</span>`, true);
+    }
 
-      comment('■Googleフォントチェック', 'Noto Sans JPの400,700または100..900、Poppinsの通常700,800,斜体700,800が必要です。');
-      const googleFonts = doc.querySelectorAll('link[href^="https://fonts.googleapis.com/"]');
-      let fontFamilyError = false;
-      if (googleFonts.length === 0) {
-        write(`<span class="red">linkタグがありません</span>`, true);
-        fontFamilyError = true;
-      }
-      if (googleFonts.length > 1) {
-        write(`<span class="red">linkタグが複数あります</span>`, true);
-        fontFamilyError = true;
-      }
-      let googleFontNotoSansOK = false;
-      let googleFontPoppinsOK = false;
-      googleFonts.forEach((e) => {
-        const url = new URL(e.href);
-        const families = url.searchParams.getAll('family');
-        for (let f of families) {
-          if (f.match('Noto Sans JP')) {
-            googleFontNotoSansOK = (f == 'Noto Sans JP:wght@400;700' || f == 'Noto Sans JP:wght@100..900');
-          }
-          if (f.match('Poppins')) {
-            googleFontPoppinsOK = (f == 'Poppins:ital,wght@0,700;0,800;1,700;1,800');
-          }
+    comment('■Googleフォントチェック', 'Noto Sans JPの400,700または100..900、Poppinsの通常700,800,斜体700,800が必要です。');
+    const googleFonts = doc.querySelectorAll('link[href^="https://fonts.googleapis.com/"]');
+    let fontFamilyError = false;
+    if (googleFonts.length === 0) {
+      write(`<span class="red">linkタグがありません</span>`, true);
+      fontFamilyError = true;
+    }
+    if (googleFonts.length > 1) {
+      write(`<span class="red">linkタグが複数あります</span>`, true);
+      fontFamilyError = true;
+    }
+    let googleFontNotoSansOK = false;
+    let googleFontPoppinsOK = false;
+    googleFonts.forEach((ele) => {
+      const url = new URL(ele.href);
+      const families = url.searchParams.getAll('family');
+      for (let f of families) {
+        if (f.match('Noto Sans JP')) {
+          googleFontNotoSansOK = (f == 'Noto Sans JP:wght@400;700' || f == 'Noto Sans JP:wght@100..900');
         }
-      });
-      if (!googleFontNotoSansOK) {
-        write(`<span class="red">Noto Sans JPの読み込みが正しくありません</span>`, true);
-        fontFamilyError = true;
-      }
-      if (!googleFontPoppinsOK) {
-        write(`<span class="red">Poppinsの読み込みが正しくありません</span>`, true);
-        fontFamilyError = true;
-      }
-      if (fontFamilyError) {
-        let linkTag = '';
-        googleFonts.forEach((e) => {
-          linkTag += e.outerHTML + '\n';
-        });
-        write(linkTag);
-      }
-      else {
-        write('<span class="blue">Googleフォント...OK</span>', true);
-      }
-
-      comment('■HTMLのコメント');
-      results = [...html.matchAll(/.*<!--[\s\S]+?-->/g)];
-      results.forEach((ele) => {
-        write(ele[0]);
-      });
-
-      comment('■CSSのコメント');
-      results = [...css.matchAll(/.*\/\*[\s\S]+?\*\//g)];
-      results.forEach((ele) => {
-        write(ele[0]);
-      });
-
-      comment('■JavaScriptのコメント');
-      results = [...js.matchAll(/.*\/\*[\s\S]+?\*\//g), ...js.matchAll(/.*\/\/.+/g)];
-      results.forEach((ele) => {
-        write(ele[0]);
-      });
-
-      comment('■meta description', '完全一致が求められています。');
-      const content = doc.querySelector('meta[name="description"]')?.content;
-      if (meta_description === content) {
-        write('description...<span class="blue">OK</span>', true);
-      }
-      else {
-        write('description...<span class="red">NG</span>', true);
-        write(content);
-      }
-
-      comment('■title', '完全一致が求められています。');
-      const title = doc.querySelector('title')?.innerText;
-      if (head_title === title) {
-        write('title...<span class="blue">OK</span>', true);
-      }
-      else {
-        write('title...<span class="red">NG</span>', true);
-        write(title);
-      }
-
-      comment('■必須タグ');
-      if (doc.querySelectorAll('header').length) {
-        write('header...<span class="blue">OK</span>', true);
-      }
-      else {
-        write('header...<span class="red">NG</span>', true);
-      }
-      if (doc.querySelectorAll('nav').length) {
-        write('nav...<span class="blue">OK</span>', true);
-      }
-      else {
-        write('nav...<span class="red">NG</span>', true);
-      }
-      if (doc.querySelectorAll('main').length) {
-        write('main...<span class="blue">OK</span>', true);
-      }
-      else {
-        write('main...<span class="red">NG</span>', true);
-      }
-      if (doc.querySelectorAll('section').length) {
-        write('section...<span class="blue">OK</span>', true);
-      }
-      else {
-        write('section...<span class="red">NG</span>', true);
-      }
-      if (doc.querySelectorAll('footer').length) {
-        write('footer...<span class="blue">OK</span>', true);
-      }
-      else {
-        write('footer...<span class="red">NG</span>', true);
-      }
-
-      comment('■見出しタグ', 'h1から始まり、順序を飛ばさずh2→h3の順で使用します。');
-      results = doc.querySelectorAll('h1,h2,h3,h4,h5,h6');
-      let h1count = doc.querySelectorAll('h1').length;
-      if (h1count === 0) {
-        write('<span class="blue">h1がありません。</span>', true);
-      }
-      else {
-        if (h1count > 1) {
-          write('<span class="blue">h1が1つではありません。</span>', true);
-        }  
-        if (results[0].tagName != 'H1') {
-          write('<span class="blue">最初がh1ではありません。</span>', true);
+        if (f.match('Poppins')) {
+          googleFontPoppinsOK = (f == 'Poppins:ital,wght@0,700;0,800;1,700;1,800');
         }
       }
-      let savedLevel = 0;
-      results.forEach((ele) => {
-        const level = ele.tagName.slice(-1) - 0;
-        const re = ele.outerHTML
-          .replaceAll('<', '&lt;').replaceAll('>', '&gt;').replace(/\n +/g, '\n')
-          .replace(/&lt;h[1-6].*?&gt;/i, '<span class="red">$&</span>')
-          .replace(/&lt;\/h[1-6]&gt;/i, '<span class="red">$&</span>');
-        const added = (savedLevel < level && ((level - savedLevel) !== 1)) ? '　<span class="blue">（飛んでいます）</span>' : '';
-        write(re + added, true);
-
-        savedLevel = level;
-      });
-      comment('■改行チェック', '文中の強制改行はNGです。');
-      results = doc.querySelectorAll('br');
-      let parentElements = [];
-      results.forEach((ele) => {
-        const pa = ele.parentElement;
-        if (parentElements.indexOf(pa) < 0) {
-          parentElements.push(pa);
-          const re = pa.outerHTML
-            .replaceAll('<', '&lt;').replaceAll('>', '&gt;').replace(/\n +/g, '\n')
-            .replace(/&lt;br.*?&gt;/ig, '<span class="red">$&</span>');
-          write(re, true);
-        }
-      });
-  
-      comment('■alt属性チェック', 'スクリーンリーダーで読ませた時に違和感が出ないようにします。文字が画像化されている時は、変更せずにaltに適用します。');
-      results = doc.querySelectorAll('img');
-      results.forEach((ele) => {
-        const re = ele.outerHTML
-          .replaceAll('<', '&lt;').replaceAll('>', '&gt;')
-          .replace(/&lt;img .+?&gt;/i, '<span class="blue">$&</span>')
-          .replace(/alt="[^"]*?"/i, '<span class="red">$&</span>');
-        write(re, true);
-      });
-
-      comment('■id属性チェック');
-      results = doc.querySelectorAll('[id]');
-      results.forEach((ele) => {
-        const re = ele.outerHTML.split('\n')[0]
-          .replaceAll('<', '&lt;').replaceAll('>', '&gt;')
-          .replace(/ id="[^"]*?"/i, '<span class="red">$&</span>');
-        write(re, true);
-      });
-
-      comment('■リンクチェック');
-      results = doc.querySelectorAll('a');
-      results.forEach((ele) => {
-        const re = ele.outerHTML
-          .replaceAll('<', '&lt;').replaceAll('>', '&gt;')
-          .replace(/&lt;a\s[\s\S]*?&gt;/i, '<span class="blue">$&</span>')
-          .replace(/ href="[^"]*"/i, '<span class="red">$&</span>');
-        write(re, true);
-      });
-
-      comment('■hoverチェック');
-      results = [...css.matchAll(/.*:hover.*/g)];
-      results.forEach((ele) => {
-        write(ele[0]);
-      });
-
-      comment('■positionプロパティチェック');
-      results = [...css.matchAll(/.*\sposition\s*:.*/g)];
-      results.forEach((ele) => {
-        write(ele[0]);
-      });
-
-      comment('■ネガティブマージンチェック', 'ネガティブマージンは極力使わないようにします。');
-      results = [...css.matchAll(/.*\s*margin[^:]*\s*:\s*-[.\d]+.*/g)];
-      results.forEach((ele) => {
-        write(ele[0]);
-      });  
-
-      comment('■transformプロパティチェック', '過度のtransformプロパティの利用がないかチェックしてください。');
-      results = [...css.matchAll(/.*\stransform\s*:.*/g)];
-      results.forEach((ele) => {
-        write(ele[0]);
-      });
-  
-      comment('■slickチェック', 'autoplayにtrueが設定されているか？');
-      if (document.getElementById('slick').value == 1) {
-        write('<span class="blue">OK</span>', true);
-      }
-      else {
-        write('<span class="red">NG</span>', true);
-      }
-  
-      comment('■チェック終了');
-      result.style.display = 'block';
     });
+    if (!googleFontNotoSansOK) {
+      write(`<span class="red">Noto Sans JPの読み込みが正しくありません</span>`, true);
+      fontFamilyError = true;
+    }
+    if (!googleFontPoppinsOK) {
+      write(`<span class="red">Poppinsの読み込みが正しくありません</span>`, true);
+      fontFamilyError = true;
+    }
+    if (fontFamilyError) {
+      let linkTag = '';
+      googleFonts.forEach((ele) => {
+        linkTag += ele.outerHTML + '\n';
+      });
+      write(linkTag);
+    }
+    else {
+      write('<span class="blue">Googleフォント...OK</span>', true);
+    }
+
+    comment('■HTMLのコメント');
+    results = [...html.matchAll(/.*<!--[\s\S]+?-->/g)];
+    results.forEach((ele) => {
+      write(ele[0]);
+    });
+
+    comment('■CSSのコメント');
+    results = [...css.matchAll(/.*\/\*[\s\S]+?\*\//g)];
+    results.forEach((ele) => {
+      write(ele[0]);
+    });
+
+    comment('■JavaScriptのコメント');
+    results = [...js.matchAll(/.*\/\*[\s\S]+?\*\//g), ...js.matchAll(/.*\/\/.+/g)];
+    results.forEach((ele) => {
+      write(ele[0]);
+    });
+
+    comment('■meta description', '完全一致が求められています。');
+    const content = doc.querySelector('meta[name="description"]')?.content?.trim();
+    if (meta_description === content) {
+      write('description...<span class="blue">OK</span>', true);
+    }
+    else {
+      write('description...<span class="red">NG</span>', true);
+      write(content);
+    }
+
+    comment('■title', '完全一致が求められています。');
+    const title = doc.querySelector('title')?.innerText?.trim();
+    if (head_title === title) {
+      write('title...<span class="blue">OK</span>', true);
+    }
+    else {
+      write('title...<span class="red">NG</span>', true);
+      write(title);
+    }
+
+    comment('■必須タグ');
+    if (doc.querySelectorAll('header').length) {
+      write('header...<span class="blue">OK</span>', true);
+    }
+    else {
+      write('header...<span class="red">NG</span>', true);
+    }
+    if (doc.querySelectorAll('nav').length) {
+      write('nav...<span class="blue">OK</span>', true);
+    }
+    else {
+      write('nav...<span class="red">NG</span>', true);
+    }
+    if (doc.querySelectorAll('main').length) {
+      write('main...<span class="blue">OK</span>', true);
+    }
+    else {
+      write('main...<span class="red">NG</span>', true);
+    }
+    if (doc.querySelectorAll('section').length) {
+      write('section...<span class="blue">OK</span>', true);
+    }
+    else {
+      write('section...<span class="red">NG</span>', true);
+    }
+    if (doc.querySelectorAll('footer').length) {
+      write('footer...<span class="blue">OK</span>', true);
+    }
+    else {
+      write('footer...<span class="red">NG</span>', true);
+    }
+
+    comment('■見出しタグ', 'h1から始まり、順序を飛ばさずh2→h3の順で使用します。');
+    results = doc.querySelectorAll('h1,h2,h3,h4,h5,h6');
+    let h1count = doc.querySelectorAll('h1').length;
+    if (h1count === 0) {
+      write('<span class="blue">h1がありません。</span>', true);
+    }
+    else {
+      if (h1count > 1) {
+        write('<span class="blue">h1が1つではありません。</span>', true);
+      }
+      if (results[0].tagName != 'H1') {
+        write('<span class="blue">最初がh1ではありません。</span>', true);
+      }
+    }
+    let savedLevel = 0;
+    results.forEach((ele) => {
+      const level = ele.tagName.slice(-1) - 0;
+      const re = ele.outerHTML
+        .replaceAll('<', '&lt;').replaceAll('>', '&gt;').replace(/\n +/g, '\n')
+        .replace(/&lt;h[1-6].*?&gt;/i, '<span class="red">$&</span>')
+        .replace(/&lt;\/h[1-6]&gt;/i, '<span class="red">$&</span>');
+      const added = (savedLevel < level && ((level - savedLevel) !== 1)) ? '　<span class="blue">（飛んでいます）</span>' : '';
+      write(re + added, true);
+
+      savedLevel = level;
+    });
+    comment('■改行チェック', '文中の強制改行はNGです。');
+    results = doc.querySelectorAll('br');
+    let parentElements = [];
+    results.forEach((ele) => {
+      const pa = ele.parentElement;
+      if (parentElements.indexOf(pa) < 0) {
+        parentElements.push(pa);
+        const re = pa.outerHTML
+          .replaceAll('<', '&lt;').replaceAll('>', '&gt;').replace(/\n +/g, '\n')
+          .replace(/&lt;br.*?&gt;/ig, '<span class="red">$&</span>');
+        write(re, true);
+      }
+    });
+
+    comment('■alt属性チェック', 'スクリーンリーダーで読ませた時に違和感が出ないようにします。文字が画像化されている時は、変更せずにaltに適用します。');
+    results = doc.querySelectorAll('img');
+    results.forEach((ele) => {
+      const re = ele.outerHTML
+        .replaceAll('<', '&lt;').replaceAll('>', '&gt;')
+        .replace(/&lt;img .+?&gt;/i, '<span class="blue">$&</span>')
+        .replace(/alt="[^"]*?"/i, '<span class="red">$&</span>');
+      write(re, true);
+    });
+
+    comment('■id属性チェック');
+    results = doc.querySelectorAll('[id]');
+    results.forEach((ele) => {
+      const re = ele.outerHTML.split('\n')[0]
+        .replaceAll('<', '&lt;').replaceAll('>', '&gt;')
+        .replace(/ id="[^"]*?"/i, '<span class="red">$&</span>');
+      write(re, true);
+    });
+
+    comment('■リンクチェック');
+    results = doc.querySelectorAll('a');
+    results.forEach((ele) => {
+      const re = ele.outerHTML
+        .replaceAll('<', '&lt;').replaceAll('>', '&gt;')
+        .replace(/&lt;a\s[\s\S]*?&gt;/i, '<span class="blue">$&</span>')
+        .replace(/ href="[^"]*"/i, '<span class="red">$&</span>');
+      write(re, true);
+    });
+
+    comment('■hoverチェック');
+    results = [...css.matchAll(/.*:hover.*/g)];
+    results.forEach((ele) => {
+      write(ele[0]);
+    });
+
+    comment('■positionプロパティチェック');
+    results = [...css.matchAll(/.*\sposition\s*:.*/g)];
+    results.forEach((ele) => {
+      write(ele[0]);
+    });
+
+    comment('■ネガティブマージンチェック', 'ネガティブマージンは極力使わないようにします。');
+    results = [...css.matchAll(/.*\s*margin[^:]*\s*:\s*-[.\d]+.*/g)];
+    results.forEach((ele) => {
+      write(ele[0]);
+    });
+
+    comment('■transformプロパティチェック', '過度のtransformプロパティの利用がないかチェックしてください。');
+    results = [...css.matchAll(/.*\stransform\s*:.*/g)];
+    results.forEach((ele) => {
+      write(ele[0]);
+    });
+
+    comment('■slickチェック', 'autoplayにtrueが設定されているか？');
+    if (document.getElementById('slick').value == 1) {
+      write('<span class="blue">OK</span>', true);
+    }
+    else {
+      write('<span class="red">NG</span>', true);
+    }
+
+    comment('■チェック終了');
+    result.style.display = 'block';
+  };
+
+  document.querySelectorAll('.perform').forEach((button) => {
+    button.addEventListener('click', buttonOnClick);
   });
 
   document.getElementById('CHECK_LIST_DOWNLOAD').addEventListener('click', (e) => {
